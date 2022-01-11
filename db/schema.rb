0891eaa6aa167be28eee3feda655758ca89798e8
nathon <nathonchavez01@gamil.com>
2022-01-12 08:16:41 +0900

    Added back database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_094220) do
+ActiveRecord::Schema.define(version: 2022_01_11_055820) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_094220) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "sessions", "users"
   add_foreign_key "tweets", "users"
 end
